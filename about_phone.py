d7e29cb1ffffcf047981191ec6b4ac1a3eca136e
shenx <lz977400292@163.com>
2018-01-21 14:28:06 +0800

    add 3 files to project

---
+++ about_phone.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+###################################################
+#           平台 Windows10 python3.6
+#           
+#
+###################################################
+
+
+import os
+class man():
+    pass
+
+class phone():
+    pass
+
+
+def Tap(x,y):
+    os.system("adb shell input tap %s %s"%(x,y))
+
+def 
+
+
+
+
